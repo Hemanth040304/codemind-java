a49c64122e688ada7ed29c0b25414c9354ec036a
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-09-20 21:45:58 +0530

    import java.util.*; class Love{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i = 0 ; i < n ; i++){             for(int j = 0 ; j < n ; j++){                 if(j==0 || i==j || i==n-1)                     System.out.format("*");                 else                     System.out.format(" ");             }             System.out.format(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -3,11 +3,12 @@ class Love{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = n ; i >= 1 ; i--){
-            for(int j = 1 ; j <= n ; j++){
-                if(i>=j){
-                    System.out.format("%d",j);
-                }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(j==0 || i==j || i==n-1)
+                    System.out.format("*");
+                else
+                    System.out.format(" ");
             }
             System.out.format("
 ");
