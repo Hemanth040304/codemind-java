a5e7b107004a13f286d47fe2a7376cba40ee3dac
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-09-29 17:06:39 +0530

    import java.util.*; class Love{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int[] arr = new int[n];         for(int i = 0 ; i < n ; i++){             arr[i] = sc.nextInt();         }         int m = sc.nextInt();         int[] brr = new int[m];         for(int i = 0 ; i < m ; i++){             brr[i] = sc.nextInt();         }         int k = sc.nextInt();         int c = 0;         for(int i = 0 ; i < n ; i++){             if(arr[i]<=k && k<=brr[i]){                 c++;             }         }         System.out.format("%d",c);     } }

---
+++ _Number_of_Students_Doing_Homework_at_a_Given_Time.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Love{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+        int[] brr = new int[m];
+        for(int i = 0 ; i < m ; i++){
+            brr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int c = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i]<=k && k<=brr[i]){
+                c++;
+            }
+        }
+        System.out.format("%d",c);
+    }
+}
