dedc54bdbd50c8af142f168954892796024a7dd8
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-10-27 21:59:44 +0530

    import java.util.*; class Code{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i = n ; i >= 1 ; i--){             for(int j = 1 ; j <= i-1 ; j++){                 System.out.printf(" ");             }             for(int k = 1 ; k <= n ; k++){                 if(i==n || i==1 || k==1 || k==n){                     System.out.format("*");                 }                 else{                     System.out.printf(" ");                 }             }             System.out.printf(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,22 @@
 import java.util.*;
-class Love{
+class Code{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = n ; i > 0 ; i--){
-            for(int j = 0 ; j < n ; j++){
-                if(i>j){
-                    System.out.printf("%c ",i+64);
+        for(int i = n ; i >= 1 ; i--){
+            for(int j = 1 ; j <= i-1 ; j++){
+                System.out.printf(" ");
+            }
+            for(int k = 1 ; k <= n ; k++){
+                if(i==n || i==1 || k==1 || k==n){
+                    System.out.format("*");
+                }
+                else{
+                    System.out.printf(" ");
                 }
             }
-            System.out.println();
+            System.out.printf("
+");
         }
     }
 }
