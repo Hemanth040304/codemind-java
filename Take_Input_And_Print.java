a92bc4f273b6f12e0f6ae1aee8ec542872c32e01
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-11-07 15:45:54 +0530

    import java.util.*; class Code{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         System.out.format("%s",s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Code{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.format("%s",s);
+    }
+}
